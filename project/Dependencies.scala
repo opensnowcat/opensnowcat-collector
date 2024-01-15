1099338e8f5c6f3f66884af57cbcc015409feb2a
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-15 17:47:09 +0000

    Update testcontainers-scala-core to 0.40.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
     // Scala (test only)
     val specs2         = "4.11.0"
     val specs2CE       = "0.4.1"
-    val testcontainers = "0.40.10"
+    val testcontainers = "0.40.17"
     val catsRetry      = "2.1.0"
     val http4s         = "0.21.33"
   }
