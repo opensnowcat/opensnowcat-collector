db40dbc7afe44518828f640181c518d97906d74e
Scala Steward <scala-steward@users.noreply.github.com>
2024-03-03 01:15:18 +0000

    Update google-cloud-pubsub to 1.127.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
   object V {
     // Java
     val awsSdk      = "1.12.666"
-    val pubsub      = "1.126.6"
+    val pubsub      = "1.127.0"
     val kafka       = "3.6.1"
     val mskAuth     = "2.0.3"
     val nsqClient   = "1.3.0"
