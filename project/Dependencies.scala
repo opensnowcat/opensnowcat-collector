c6308311b7d00557980c2dea11074cd5dd9f16f2
Scala Steward <scala-steward@users.noreply.github.com>
2024-05-19 01:22:22 +0000

    Update aws-java-sdk-kinesis, ... to 1.12.725

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
 
   object V {
     // Java
-    val awsSdk      = "1.12.696"
+    val awsSdk      = "1.12.725"
     val pubsub      = "1.128.1"
     val kafka       = "3.7.0"
     val mskAuth     = "2.1.0"
