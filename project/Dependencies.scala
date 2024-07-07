24a03d659b524776cd599a4d62ba262aec5111f8
Scala Steward <scala-steward@users.noreply.github.com>
2024-07-07 01:29:16 +0000

    Update specs2-core to 4.20.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
     val akkaHttpMetrics  = "1.7.1"
     val badRows          = "2.1.2"
     // Scala (test only)
-    val specs2         = "4.20.5"
+    val specs2         = "4.20.8"
     val specs2CE       = "0.5.4"
     val testcontainers = "0.41.3"
     val catsRetry      = "2.1.1"
