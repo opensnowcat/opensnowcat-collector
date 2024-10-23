86fabc909b6a4ca49f3398ab271bc6771a144e16
Oleh Yermolaiev <oleg.yermolaiev@sysgears.com>
2024-10-23 17:49:53 +0300

    Update apache libthrift to 0.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
     val config      = "1.4.3"
     val rabbitMQ    = "5.21.0"
     val jackson     = "2.17.2"
-    val thrift      = "0.15.0"
+    val thrift      = "0.16.0"
     val jnrUnixsock = "0.38.22"
     val protobuf    = "3.25.5"
     val guava       = "32.1.3-jre"
