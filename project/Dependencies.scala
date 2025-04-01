10e2ae7ae4205add743e2c9c5e9af748b1e17acc
Oleh Yermolaiev <oermolaev@users.noreply.github.com>
2025-04-01 08:37:36 +0300

    Update netty-all to patch CVE-2025-24970 (#168)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
     val jnrUnixsock = "0.38.22"
     val protobuf    = "3.25.5"
     val guava       = "32.1.3-jre"
-    val nettyAll    = "4.1.115.Final"
+    val nettyAll    = "4.1.119.Final"
 
     // Scala
     val collectorPayload = "0.0.0"
