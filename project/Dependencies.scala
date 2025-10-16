3c95528c1fa39f147d4e7276540d66fb4d0cc59c
Oleh Yermolaiev <oleg.yermolaiev@sysgears.com>
2025-10-16 14:45:16 +0300

    Update netty-all to 4.1.127.Final to resolve CVE in smtp-codec

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
     val jnrUnixsock = "0.38.22"
     val protobuf    = "3.25.5"
     val guava       = "32.1.3-jre"
-    val nettyAll    = "4.1.126.Final"
+    val nettyAll    = "4.1.127.Final"
     val commonsLang = "3.18.0"
 
     // Scala
