37dcd1fb31b44c0278fa8dfaa717ca12f3d5465b
Oleh Yermolaiev <oleg.yermolaiev@sysgears.com>
2026-03-13 16:49:17 +0200

    Update lz4-java to 1.10.4 version to patch CVE-2025-66566

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     val awsSdkV2    = "2.42.4"
     val pubsub      = "1.132.4"
     val kafka       = "3.9.1"
-    val lz4Java     = "1.8.1"
+    val lz4Java     = "1.10.4"
     val mskAuth     = "2.2.0"
     val nsqClient   = "1.3.0"
     val jodaTime    = "2.12.7"
